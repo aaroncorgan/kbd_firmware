640c9205e91c928a879d8702aac19094808ac2d5
Aaron Reeves <aaron.reeves@corgan.com>
2024-06-06 16:30:16 +0000

    Jun 6, 2024, 11:30 AM

--- keyboards/crkbd/vial-kb/vial-qmk/keymaps/vial/keymap.c
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/vial/keymap.c
@@ -6,7 +6,6 @@ enum custom_keycodes {
     RM_RBRC,
     RM_PGUP,
     RM_PGDN,
-    TD_MKEY,
 };
 
 enum {
@@ -74,160 +73,29 @@ void matrix_scan_user(void) {
     }
 }
 
-void dance_finished(tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            tap_code16(KC_K); // K
-            break;
-        case 2:
-            tap_code16(LALT(KC_LSFT)); // ALT+SHIFT+K
-            break;
-        case 3:
-            tap_code16(LCTL(KC_LSFT)); // CTRL+SHIFT+ESC
-            break;
-    }
-}
-
-void dance_reset(tap_dance_state_t *state, void *user_data) {
-    // Reset any persistent state if needed
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_MKEY_ACTION] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_finished, dance_reset),
-};
-
-uint16_t COMBO_LEN = COMBO_LENGTH;
-
-const uint16_t PROGMEM combo_q_tilde[] = {KC_Q, KC_PLUS, COMBO_END};
-const uint16_t PROGMEM combo_qw_1[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_we_2[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM combo_er_3[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM combo_rt_4[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_tab_a_shift_tab[] = {KC_TAB, KC_A, COMBO_END};
-const uint16_t PROGMEM combo_zx_ctrl_z[] = {KC_Z, KC_X, COMBO_END};
-const uint16_t PROGMEM combo_xc_ctrl_x[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM combo_cv_ctrl_c[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM combo_vb_ctrl_v[] = {KC_V, KC_B, COMBO_END};
-const uint16_t PROGMEM combo_yu_lbracket[] = {KC_Y, KC_U, COMBO_END};
-const uint16_t PROGMEM combo_ui_rbracket[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM combo_io_lparen[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM combo_op_rparen[] = {KC_O, KC_P, COMBO_END};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-    switch(combo_index) {
-        case COMBO_Q_TILDE:
-            if (pressed) {
-                tap_code16(KC_GRV); // ` (Grave accent, usually for ~)
-            }
-            break;
-        case COMBO_QW_1:
-            if (pressed) {
-                tap_code(KC_1);
-            }
-            break;
-        case COMBO_WE_2:
-            if (pressed) {
-                tap_code(KC_2);
-            }
-            break;
-        case COMBO_ER_3:
-            if (pressed) {
-                tap_code(KC_3);
-            }
-            break;
-        case COMBO_RT_4:
-            if (pressed) {
-                tap_code(KC_4);
-            }
-            break;
-        case COMBO_TAB_A_SHIFT_TAB:
-            if (pressed) {
-                tap_code16(S(KC_TAB));
-            }
-            break;
-        case COMBO_ZX_CTRL_Z:
-            if (pressed) {
-                tap_code16(C(KC_Z));
-            }
-            break;
-        case COMBO_XC_CTRL_X:
-            if (pressed) {
-                tap_code16(C(KC_X));
-            }
-            break;
-        case COMBO_CV_CTRL_C:
-            if (pressed) {
-                tap_code16(C(KC_C));
-            }
-            break;
-        case COMBO_VB_CTRL_V:
-            if (pressed) {
-                tap_code16(C(KC_V));
-            }
-            break;
-        case COMBO_YU_LBRACKET:
-            if (pressed) {
-                tap_code(KC_LBRC);
-            }
-            break;
-        case COMBO_UI_RBRACKET:
-            if (pressed) {
-                tap_code(KC_RBRC);
-            }
-            break;
-        case COMBO_IO_LPAREN:
-            if (pressed) {
-                tap_code16(S(KC_9)); // (
-            }
-            break;
-        case COMBO_OP_RPAREN:
-            if (pressed) {
-                tap_code16(S(KC_0)); // )
-            }
-            break;
-    }
-}
-
-combo_t key_combos[] = {
-    [COMBO_Q_TILDE] = COMBO_ACTION(combo_q_tilde),
-    [COMBO_QW_1] = COMBO_ACTION(combo_qw_1),
-    [COMBO_WE_2] = COMBO_ACTION(combo_we_2),
-    [COMBO_ER_3] = COMBO_ACTION(combo_er_3),
-    [COMBO_RT_4] = COMBO_ACTION(combo_rt_4),
-    [COMBO_TAB_A_SHIFT_TAB] = COMBO_ACTION(combo_tab_a_shift_tab),
-    [COMBO_ZX_CTRL_Z] = COMBO_ACTION(combo_zx_ctrl_z),
-    [COMBO_XC_CTRL_X] = COMBO_ACTION(combo_xc_ctrl_x),
-    [COMBO_CV_CTRL_C] = COMBO_ACTION(combo_cv_ctrl_c),
-    [COMBO_VB_CTRL_V] = COMBO_ACTION(combo_vb_ctrl_v),
-    [COMBO_YU_LBRACKET] = COMBO_ACTION(combo_yu_lbracket),
-    [COMBO_UI_RBRACKET] = COMBO_ACTION(combo_ui_rbracket),
-    [COMBO_IO_LPAREN] = COMBO_ACTION(combo_io_lparen),
-    [COMBO_OP_RPAREN] = COMBO_ACTION(combo_op_rparen)
-};
-
 #ifdef LAYOUT_split_3x6_3_ex2
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-    KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, RM_LBRC,    RM_PGUP,    KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,   KC_BSPC,
+    KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, RM_LBRC,       RM_PGUP,   KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-    LT(1,KC_TAB),   KC_A,   KC_S,    KC_D,  KC_F, KC_G, LT(2, RM_RBRC),    TD_MKEY,   KC_H,    KC_J,  KC_K,  KC_L, KC_SCLN,  LT(4,KC_ENT),
+    LT(1,KC_TAB), KC_A, KC_S,  KC_D,  KC_F, KC_G, LT(2, RM_RBRC),       RM_PGDN,   KC_H,    KC_J,   KC_K,   KC_L,  KC_SCLN,   KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-        SC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+        SC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                            KC_LCTL, KC_LALT, KC_SPC,  LT(3,KC_SPC), KC_LSFT, KC_F24
+                                            KC_LCTL, KC_LALT, KC_SPC,  LT(3,KC_SPC), KC_RSFT, MT(4,KC_F24)
                                       //`--------------------------'  `--------------------------'
 ),
 
 [1] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-        KC_ESC,  KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,  KC_TRNS,     KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+        KC_ESC,  KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,  KC_TRNS,      KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        KC_TRNS,   KC_H,    KC_J,   KC_K,   KC_L, KC_SCLN,  KC_TRNS,     KC_TRNS, KC_PLUS,  KC_DLR, KC_PERC, KC_CIRC, KC_DQUO, KC_TRNS,
+        KC_TRNS,   KC_H,    KC_J,   KC_K,   KC_L, KC_SCLN,  KC_TRNS,     KC_TRNS, KC_PLUS,  KC_DLR,KC_PERC, KC_CIRC, KC_DQUO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-        KC_TRNS,   KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,                       KC_PIPE, KC_EXLM,  KC_AT, KC_HASH, KC_TILD, KC_TRNS,
+        KC_TRNS,   KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,                      KC_PIPE, KC_EXLM,  KC_AT, KC_HASH, KC_TILD, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                            KC_TRNS, KC_TRNS, KC_TRNS,    KC_UNDS, KC_LPRN, KC_RPRN
+                                            KC_TRNS, KC_TRNS, KC_TRNS,   KC_UNDS, KC_LPRN, KC_RPRN
                                       //`--------------------------'  `--------------------------'
 ),
 
